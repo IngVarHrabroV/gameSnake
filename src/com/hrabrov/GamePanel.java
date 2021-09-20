2d941dd2b2441845dda94a7f18eee6b5884625fe
IngVarHrabroV <ingvarhabrov@gmail.com>
2021-09-20 14:10:15 +0300

    add grid in game window

--- src/com/hrabrov/GamePanel.java
+++ src/com/hrabrov/GamePanel.java
@@ -42,10 +42,16 @@ public class GamePanel extends JPanel implements ActionListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
+
     }
 
     public void draw(Graphics g) {
+        g.setColor(Color.darkGray);
 
+        for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
     }
 
     public void newApple(){

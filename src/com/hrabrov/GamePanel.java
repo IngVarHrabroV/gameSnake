1b38b249ea5535aae0c2f52b43bace6f9422f8a0
IngVarHrabroV <ingvarhabrov@gmail.com>
2021-09-20 17:43:07 +0300

    add snake's body and move's method

--- src/com/hrabrov/GamePanel.java
+++ src/com/hrabrov/GamePanel.java
@@ -3,14 +3,13 @@ package com.hrabrov;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
-    static final int SCREEN_WIDTH = 600;
-    static final int SCREEN_HEIGHT = 600;
+    static final int SCREEN_WIDTH_SIZE = 600;
+    static final int SCREEN_HEIGHT_SIZE = 600;
     static final int UNIT_SIZE = 25;
-    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    static final int GAME_UNITS = (SCREEN_WIDTH_SIZE * SCREEN_HEIGHT_SIZE) / UNIT_SIZE;
     static final int DELAY = 75;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
@@ -25,11 +24,39 @@ public class GamePanel extends JPanel implements ActionListener {
 
     GamePanel() {
         random = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
-        this.setBackground(Color.black);
+        this.setBackground(Color.gray);
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH_SIZE, SCREEN_HEIGHT_SIZE));
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
         startGame();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.lightGray);
+        for (int i = 0; i < SCREEN_HEIGHT_SIZE / UNIT_SIZE; i++) {
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT_SIZE);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH_SIZE, i * UNIT_SIZE);
+        }
+
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i = 0; i < bodyParts; i++) {
+            if ( i == 0) {
+                g.setColor(Color.blue);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            } else {
+                g.setColor(new Color(20, 66, 2));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+
+
+        }
     }
 
     public void startGame() {
@@ -39,26 +66,31 @@ public class GamePanel extends JPanel implements ActionListener {
         timer.start();
     }
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        draw(g);
-
-    }
-
-    public void draw(Graphics g) {
-        g.setColor(Color.darkGray);
-
-        for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
-            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
-            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
-        }
-    }
-
     public void newApple(){
-
+        appleX = random.nextInt((int)(SCREEN_WIDTH_SIZE / UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT_SIZE / UNIT_SIZE)) * UNIT_SIZE;
     }
 
     public void move() {
+        for(int i = bodyParts; i > 0; i--){
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
 
     }
 

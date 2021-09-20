30fdb5000dbaec3cdaf5d4209ebf8d6ea9c4ae70
IngVarHrabroV <ingvarhabrov@gmail.com>
2021-09-20 13:39:07 +0300

    create game window

---
+++ src/com/hrabrov/GameFrame.java
@@ -0,0 +1,15 @@
+package com.hrabrov;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    GameFrame () {
+        add(new GamePanel());
+        setTitle("Snake Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        pack();
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+}
